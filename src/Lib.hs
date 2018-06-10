85b3e7c2988a0492cb460c966e4bac4b7edd87b7
michael <mwhitehead@sofi.org>
2018-06-09 20:29:40 -0600

    playing around with an idea

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,42 +1,89 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Lib
     (
     ) where
 
+import           Control.Concurrent
 import           Control.Concurrent.STM
+import           Control.Concurrent.STM.TMChan
 import           Control.Exception              (finally)
 import           Control.Monad                  (forever)
+import           Data.Aeson
 import qualified Data.ByteString.Lazy           as BS
+import qualified Data.Map                       as M
+import           Data.Maybe
+import qualified Data.Text                      as T
+import           Data.UUID
+import           Data.UUID.V4
+import           GHC.Generics
 import qualified Network.Wai
 import qualified Network.Wai.Handler.WebSockets as WaiWS
 import qualified Network.WebSockets             as WS
 
-wsApp :: WS.ServerApp
-wsApp pending = do
+wsApp :: QuerySubscriptionState -> WS.ServerApp
+wsApp subState pending = do
   conn <- WS.acceptRequest pending
   WS.forkPingThread conn 30
-  finally (connect conn) disconnect
+  finally (connect subState conn) disconnect
   where
     connect = undefined
     disconnect = undefined
 
-type QueryId = String
+type QueryId = UUID
+type ConnId = UUID
 
-data CQRSOps = JoinQuery BS.ByteString
+data CQRSOps = JoinQuery T.Text
              | LeaveQuery QueryId
-             | Write BS.ByteString
+             | Write T.Text
+             deriving (Generic)
 
-connect conn = forever $ do
+data QueryRes = Res { queryId  :: QueryId
+                    , queryRes :: BS.ByteString
+                    }
+              | Joined QueryId
+              | Left QueryId
+
+
+instance FromJSON CQRSOps
+
+connect subState conn = forever $ do
   msg <- WS.receiveData conn
-  putStrLn $ show (msg :: BS.ByteString)
-  -- connect to query
-  -- leave query
-  -- handle write
+  let (Just op) = decode msg :: Maybe CQRSOps
+  case op of
+    JoinQuery q    -> do
+      connId <- nextRandom
+      (qId, chan) <- joinQuery q connId subState
+      forward conn qId chan
+    LeaveQuery qId -> return ()
+    Write v        -> return ()
 
-data QuerySubscriptionState
+  where
+    forward conn qId chan = do
+      output <- atomically $ readTMChan chan
+      case output of
+        Just res -> do
+          WS.sendTextData conn res
+          forward conn qId chan
+        Nothing -> return ()
 
-joinQuery :: BS.ByteString -> QuerySubscriptionState -> STM QueryId
-joinQuery = undefined
+type QuerySubscriptionState = TVar (M.Map ConnId (M.Map QueryId (TMChan BS.ByteString)))
+
+joinQuery :: T.Text -> ConnId -> QuerySubscriptionState -> IO (QueryId, (TMChan BS.ByteString))
+joinQuery query connId subState = do
+  chan <- newTMChanIO
+  qId <- nextRandom
+  atomically $ do
+    modifyTVar subState (M.alter (Just . maybe M.empty (M.insert qId chan)) connId)
+
+  return (qId, chan)
+
+
+
 
 leaveQuery :: QueryId -> QuerySubscriptionState -> STM ()
 leaveQuery = undefined
+
+disconnect :: ConnId -> QuerySubscriptionState -> STM ()
+disconnect = undefined
 
